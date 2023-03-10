dc9ef343deb7f0f665842244232e872e57392fb9
Jo Grisold <jo.grisold@gmail.com>
2023-03-10 15:51:22 -0500

    setHeader

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 /** Setting up server to accept cross-origin browser requests */
 app.use((req, res, next)=> { //allow cross origin requests
   res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
-  res.header("Access-Control-Allow-Origin", "https://btb.ltd/");
+  res.setHeader("Access-Control-Allow-Origin", "https://btb.ltd/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", true);
   next();
